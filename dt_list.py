bc2af60fbe2cd7a952ae9588cc1dc8ea83508a83
philane-msibi-ndlondlo <philanemsibi14@gmail.com>
2021-11-28 13:53:15 +0200

    Added Type forcing on dt list init and length, remove_at

--- dt_list.py
+++ dt_list.py
@@ -3,39 +3,62 @@ This class defines the Dt List for enforcing specific data types on a list
 '''
 class Dt_list:
 
-    __root_list = []
-    __data_type = None
-
     def __init__(self, data_type, n_list) -> None:
+        self.__data_type = None
+        self.__root_list = []
+        self.length = 0
+
         self.__data_type = data_type
-        self.__root_list = n_list
 
-    def is_valid_list(self, m_list):
-        pass
+        for item in n_list:
+            if self.is_valid_data_type(item):
+                 self.add(item)
+            else:
+                print(f'Error: {item} not of specified dt type')
+                exit(1)
+        self.length = len(self.__root_list)
 
+    def is_valid_data_type(self, item):
 
+        try:
+           if self.__data_type == "int" and type(item) is int:
+             item = int(item)
+           elif self.__data_type == "str" and type(item) is str:
+             item = str(item)
+           elif self.__data_type == "float" and type(item) is float:
+             item = float(item)
+           elif self.__data_type == "bool" and type(item) is bool:
+             item = bool(item)
+           else:
+             return False
+           return True
+
+        except:
+            return False
+
+    def __cast(self, item):
+        if self.__data_type == "int":
+             item = int(item)
+        elif self.__data_type == "str":
+             item = str(item)
+        elif self.__data_type == "float":
+             item = float(item)
+        elif self.__data_type == "bool":
+             item = bool(item)
+        else:
+            pass       
+
+        return item 
 
     def add(self, item) -> None:
 
-        try:
-            if self.__data_type == "int":
-                item = int(item)
-            elif self.__data_type == "str":
-                item = str(item)
-            elif self.__data_type == "float":
-                item = float(item)
-            elif self.__data_type == "bool":
-                item = bool(item)
-            else:
-                print("No Data Type Matches")
-                exit(1)
+        if self.is_valid_data_type(item):
+            self.__root_list.append(self.__cast(item))
+            self.length += 1
+        else:
+            print(f'Error: {item}. Invalid Type for dt list')
+            exit(1) 
 
-            self.__root_list.append(item)
-
-        except:
-            print(f'{item} is not of type {self.__data_type}')
-            exit(1)
-        
     def __str__(self) -> str:
         output = "["
         for index, num in enumerate(self.__root_list):
@@ -45,3 +68,18 @@ class Dt_list:
                  output += f'{num}'
         output += "]"
         return output
+
+    def length(self):
+        return self.length
+
+    def is_empty(self):
+        return self.length == 0
+
+    def remove_at(self, index):
+        
+        if index >= 0 and index < self.length -1:
+            self.__root_list = self.__root_list[:index] + self.__root_list[index+1:]
+
+
+
+
--- main.py
+++ main.py
@@ -6,8 +6,12 @@ if __name__ == "__main__":
     my_list.add(23)
     print(my_list)
 
+    my_list.remove_at(6)
+    print(my_list)
+
     my_list2 = Dt_list('str', [])
-    my_list2.add(2)
+    my_list2.add('2')
     my_list2.add("string")
 
     print(my_list2)
+    print(my_list.length)
